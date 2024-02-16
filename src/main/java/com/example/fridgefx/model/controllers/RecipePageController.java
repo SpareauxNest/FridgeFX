af03b3a6c27a6a5a49a021dd9af1ee6ab1a6631c
Spareaux <spareauxnest@gmail.com>
2024-02-16 12:44:43 -0500

    God I hate imports

--- src/main/java/com/example/fridgefx/model/controllers/RecipePageController.java
+++ src/main/java/com/example/fridgefx/model/controllers/RecipePageController.java
@@ -1,4 +1,11 @@
 package com.example.fridgefx.model.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.web.*;
+
 public class RecipePageController {
-}
+
+    @FXML
+    private TextField recipeTitle;
+
