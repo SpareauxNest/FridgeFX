5fb0e727807ecbee8fb52d2e7c28e2b0ea2e306b
Spareaux <spareauxnest@gmail.com>
2024-02-16 09:54:55 -0500

    Recipes now get added!

--- src/main/java/com/example/fridgefx/model/controllers/AddRecipeController.java
+++ src/main/java/com/example/fridgefx/model/controllers/AddRecipeController.java
@@ -1,16 +1,31 @@
 package com.example.fridgefx.model.controllers;
 
+import com.example.fridgefx.model.Ingredient;
+import com.example.fridgefx.model.Recipe;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.util.List;
 
 public class AddRecipeController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    private List<Ingredient> ingredients;
+    private List<Recipe> recipes;
+
+    @FXML
+    private TextField recipeNameField;
+
+    @FXML
+    private TextField recipeLinkField;
 
     public void AddNewIngredient(ActionEvent event) {
         try {
@@ -48,6 +63,10 @@ public class AddRecipeController {
             root = loader.load();
             ListViewController listViewController = loader.getController();
 
+            recipes.add(new Recipe(recipeNameField.getText() , ingredients, recipeLinkField.getText()));
+
+            listViewController.RecipeSubmit(recipes);
+
             stage = (Stage)((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
             stage.setScene(scene);
@@ -57,4 +76,7 @@ public class AddRecipeController {
         }
     }
 
+    public void sendListToRecipeController(List<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 }
--- src/main/java/com/example/fridgefx/model/controllers/ListViewController.java
+++ src/main/java/com/example/fridgefx/model/controllers/ListViewController.java
@@ -1,5 +1,6 @@
 package com.example.fridgefx.model.controllers;
 
+import com.example.fridgefx.model.Recipe;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,16 +10,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.SelectionMode;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ListViewController implements Initializable {
 
     @FXML
-    ListView listView;
+    ListView<String> listView = new ListView<>();
 
     @FXML
     Button newRecipeButton;
@@ -26,9 +29,15 @@ public class ListViewController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    private List<Recipe> recipes = new ArrayList<>();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        recipes.stream().forEach(recipe -> {
+            listView.getItems().add(recipe.getName());
+        });
+        //listView.getItems().add(recipes.stream().map(Recipe::getName).toString());
+        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
 
     public void AddNewRecipe(ActionEvent event) {
@@ -37,6 +46,8 @@ public class ListViewController implements Initializable {
             root = loader.load();
             AddRecipeController addRecipeController = loader.getController();
 
+            addRecipeController.sendListToRecipeController(recipes);
+
             stage = (Stage)((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
             stage.setScene(scene);
@@ -44,6 +55,13 @@ public class ListViewController implements Initializable {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void RecipeSubmit(List<Recipe> recipes){
+        this.recipes = recipes;
+        recipes.stream().forEach(recipe -> {
+            listView.getItems().add(recipe.getName());
+        });
     }
 
 }
