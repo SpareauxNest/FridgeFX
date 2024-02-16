7ddb69c9d52da2b837d890338430f2e9a47fab40
Spareaux <spareauxnest@gmail.com>
2024-02-16 16:25:15 -0500

    Set up treeview shell

--- src/main/java/com/example/fridgefx/model/controllers/RecipePageController.java
+++ src/main/java/com/example/fridgefx/model/controllers/RecipePageController.java
@@ -1,11 +1,27 @@
 package com.example.fridgefx.model.controllers;
 
+import com.example.fridgefx.model.Ingredient;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
-import javafx.scene.web.*;
+import javafx.scene.control.TreeView;
 
-public class RecipePageController {
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class RecipePageController implements Initializable {
 
     @FXML
     private TextField recipeTitle;
 
+    @FXML
+    private TreeView treeView;
+
+    private List<Ingredient> ingredients;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
