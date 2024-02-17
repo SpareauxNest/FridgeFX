3f14735c2eefbf3109bdf3397cb54bc7b73da271
Spareaux <spareauxnest@gmail.com>
2024-02-16 19:58:43 -0500

    AddIngredient now works!

--- src/main/java/com/example/fridgefx/model/controllers/AddRecipeController.java
+++ src/main/java/com/example/fridgefx/model/controllers/AddRecipeController.java
@@ -5,21 +5,27 @@ import com.example.fridgefx.model.Recipe;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class AddRecipeController {
+public class AddRecipeController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    private List<Ingredient> ingredients;
-    private List<Recipe> recipes;
+    private List<Ingredient> ingredients = new ArrayList<>();
+    private List<Recipe> recipes = new ArrayList<>();
 
     @FXML
     private TextField recipeNameField;
@@ -27,8 +33,26 @@ public class AddRecipeController {
     @FXML
     private TextField recipeLinkField;
 
+    @FXML
+    private ListView ingredientListView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ingredients.stream().forEach(ingredient -> {
+            ingredientListView.getItems().add(ingredient.getName());
+        });
+        ingredientListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
     public void sendListToRecipeController(List<Recipe> recipes) {
         this.recipes = recipes;
+    }
+    public void sendListToRecipeController(List<Recipe> recipes, List<Ingredient> ingredients) {
+        this.recipes = recipes;
+        this.ingredients = ingredients;
+        ingredients.stream().forEach(ingredient -> {
+            ingredientListView.getItems().add(ingredient.getName());
+        });
     }
 
     public void AddNewIngredient(ActionEvent event) {
@@ -36,6 +60,7 @@ public class AddRecipeController {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/fridgefx/AddIngredient.fxml"));
             root = loader.load();
             AddIngredientController addIngredientController = loader.getController();
+            addIngredientController.sendDataToIngredient(recipes, ingredients);
 
             stage = (Stage)((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
